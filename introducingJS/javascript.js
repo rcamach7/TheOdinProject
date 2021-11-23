4d3f5ff4cf7b51303b742469dbaaf8998fa70a0b
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2021-11-22 20:47:54 -0800

    introduced javascript inside HTML and some CSS Flexbox modeling

---
+++ introducingJS/javascript.js
@@ -0,0 +1,13 @@
+
+// My first dynamic button
+const utton = document.getElementById('myBu');
+console.log(utton.textContent);
+// button.addEventListener('click', updateButton);
+
+// function updateButton() {
+//     if (button.textContent == 'Start World') {
+//         button.textContent = 'World Started! Click to Stop';
+//     } else {
+//         button.textContent = 'Start World';
+//     }
+// }
